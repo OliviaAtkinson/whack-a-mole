c83f4f2103b65f780816e5ef8d0d09a96ab15ca0
Codenation11 <Codenation11@Codenation10s-MacBook-Pro-3.local>
2018-11-08 10:22:21 +0000

    updated js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const holes = document.querySelectorAll('.field');
 const scoreBoard = document.querySelector('.score');
 const mrBumps = document.querySelectorAll('.mrbump');
 let score = 0;
-let timeUp = false;
-let lastHole;
 let prevNum = -1;
+let isPlaying = false;
 
 function startGame() {
-    timeUp = false;
-    score = 0;
-    let intervalId = setInterval(newBump, 3000);
-    setTimeout(endGame, 30000, intervalId);
+    if(!isPlaying) {
+        isPlaying = true
+        score = 0;
+        let intervalId = setInterval(newBump, 1500);
+        setTimeout(endGame, 30000, intervalId);    
+    }
 }
-
 function newBump() {
     let randBump = Math.floor(Math.random() * 6);
     
@@ -23,14 +23,22 @@ function newBump() {
         mrBumps[randBump].style.visibility = "visible";
     }
     prevNum = randBump;
-    setTimeout(reset, 2500, randBump);
+    setTimeout(reset, 1000, randBump);
 }
-
 function reset(randBump) {
     mrBumps[randBump].style.visibility = "hidden";
 }
-
 function endGame(intervalId) {
     alert('Time Up!');
     clearInterval(intervalId);
+    isPlaying = false;
 }
+function clickBump (){
+    // let clickingMrBumps = document.addEventListener(".mrbump");
+  
+    this.parentNode.classList.remove('up');
+    score+= 1;
+    scoreBoard.innerHTML = score;
+
+}
+mrBumps.forEach(mrbump => mrbump.addEventListener('click', clickBump));
